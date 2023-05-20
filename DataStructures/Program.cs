40847381dd7f0b4b72fea62a73ae50fbfba62366
Michael Schultz <mikewschultz9029@gmail.com>
2023-05-20 14:39:22 -0400

    played with dictionaries

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -64,6 +64,22 @@ namespace DataStructures
             };
             // accessing element in multi-dimensional array
             Console.WriteLine(my2DArray[0, 2]);
+
+            // * -------- DICTIONARY --------
+
+            // initialize empty dictionary with string keys and int values
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+            // map key of "hello" to value of 75
+            myDictionary["hello"] = 75;
+            Console.WriteLine(myDictionary["hello"]);
+
+            // initialize dictionary with one value with key of "Dairy" with a value of a string array with values of 0: "Milk" and 1: "Cheese"
+            Dictionary<string, string[]> myGroceryDictionary = new Dictionary<string, string[]>() {
+                { "Dairy", new string[]{"Milk", "Cheese"} }
+            };
+            // set new key of "Meat" to an array with values 0: "Chicken" and 1: "Beef"
+            myGroceryDictionary["Meat"] = new string[] { "Chicken", "Beef" };
+            Console.WriteLine(myGroceryDictionary["Meat"][0]);
         }
     }
 }
