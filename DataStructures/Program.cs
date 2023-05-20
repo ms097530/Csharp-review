dab8e7a0be2bb5ad1e8987231d6ed68f6ef14dae
Michael Schultz <mikewschultz9029@gmail.com>
2023-05-20 14:08:52 -0400

    started with lists

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -6,6 +6,10 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            // * -------- ARRAYS --------
+
+            // ? Arrays do not grow or shrink, uninitialized space will be some sort of default value
+
             string[] myGroceryArray = new string[2];
             myGroceryArray[0] = "Apples";
             myGroceryArray[1] = "Baguettes";
@@ -16,6 +20,26 @@ namespace DataStructures
             {
                 Console.WriteLine(grocery);
             }
+
+            // initialize C# array
+            int[] myIntArray = { 0, 1, 2 };
+
+            int[] myEmptyArray = new int[3];
+
+            myEmptyArray.Append(69);
+            foreach (int num in myEmptyArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            // * -------- LISTS --------
+
+            List<int> myGroceryList = new List<int>(69);
+            // number of items inside
+            System.Console.WriteLine(myGroceryList.Count);
+            // number of POSSIBLE items inside
+            System.Console.WriteLine(myGroceryList.Capacity);
+
         }
     }
 }
