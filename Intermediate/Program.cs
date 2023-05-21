299a555bee03f1a471192929a2f08aae150d6cbd
Michael Schultz <mikewschultz9029@gmail.com>
2023-05-21 12:50:40 -0400

    CLEANED UP FOR FILE MANIPULATION

--- Intermediate/Program.cs
+++ Intermediate/Program.cs
@@ -12,23 +12,6 @@ namespace Intermediate
     {
         static void Main(string[] args)
         {
-            // gives access to this file - could do similarly for other files or settings
-            IConfiguration config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            DataContextDapper dapper = new DataContextDapper(config);
-            DataContextEF entityFramework = new DataContextEF(config);
-
-            string sqlCommand = "SELECT GETDATE()";
-
-            // Query from Dapper
-            // Query will return array of results
-            // QuerySingle returns one result
-            // DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
-            DateTime rightNow = dapper.LoadDataSingle<DateTime>(sqlCommand);
-
-            Console.WriteLine(rightNow);
 
             Computer myComputer = new Computer()
             {
@@ -39,10 +22,6 @@ namespace Intermediate
                 Price = 943.87m,
                 VideoCard = "RTX 2060"
             };
-
-            // accomplishes same thing as below SQL statement being executed
-            entityFramework.Add(myComputer);
-            entityFramework.SaveChanges();
 
             string sql =
                 @"INSERT INTO TutorialAppSchema.Computer (
@@ -61,56 +40,7 @@ namespace Intermediate
                 + "','" + myComputer.VideoCard
                 + "')";
 
-            // returns whether the executed sql affected anything
-            bool result = dapper.ExecuteSql(sql);
 
-            Console.WriteLine(result);
-
-            string sqlSelect = @"SELECT
-                Computer.ComputerId, 
-                Computer.Motherboard,
-                Computer.HasWifi,
-                Computer.HasLTE,
-                Computer.ReleaseDate,
-                Computer.Price,
-                Computer.VideoCard
-             FROM TutorialAppSchema.Computer";
-
-            // returns IEnumerable of type we used
-            // could convert to List by adding ToList() method at the end
-            IEnumerable<Computer> computers = dapper.LoadData<Computer>(sqlSelect);
-            foreach (Computer computer in computers)
-            {
-                Console.WriteLine("'" + computer.ComputerId
-                + "','" + computer.Motherboard
-                + "','" + computer.HasWifi
-                + "','" + computer.HasLTE
-                + "','" + computer.ReleaseDate
-                + "','" + computer.Price
-                + "','" + computer.VideoCard
-                + "')");
-            }
-
-            IEnumerable<Computer>? computersEf = entityFramework.Computer?.ToList<Computer>();
-            if (computersEf != null)
-            {
-                foreach (Computer computer in computersEf)
-                {
-                    Console.WriteLine("'" + computer.ComputerId
-                    + "','" + computer.Motherboard
-                    + "','" + computer.HasWifi
-                    + "','" + computer.HasLTE
-                    + "','" + computer.ReleaseDate
-                    + "','" + computer.Price
-                    + "','" + computer.VideoCard
-                    + "')");
-                }
-            }
-
-
-            // Console.WriteLine(myComputer.Motherboard);
-
-            // Console.WriteLine("end of file");
         }
     }
 }
