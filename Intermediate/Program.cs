bfbca2e172aca86bcade6a6cbf6e8b01dd8cef8b
Michael Schultz <mikewschultz9029@gmail.com>
2023-05-21 16:23:51 -0400

    began setting up auto mapping

--- Intermediate/Program.cs
+++ Intermediate/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using System.Text.Json;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using AutoMapper;
 
 namespace Intermediate
 {
@@ -65,67 +66,72 @@ namespace Intermediate
             // Console.WriteLine(fileText);
 
             // * stored in string as is, can't access key-value pairs or access like model
-            string computersJson = File.ReadAllText("Computers.json");
+            string computersJson = File.ReadAllText("ComputersSnake.json");
 
-            // Console.WriteLine(computersJson);
-            // * --- SYSTEM
-            // * maps 1 to 1 by default, change naming policy
-            // * i.e. default: in file property = motherboard, looks to map to model as motherboard (rather than Motherboard)
-            JsonSerializerOptions options = new JsonSerializerOptions()
+            Mapper mapper = new Mapper(new MapperConfiguration((cfg) =>
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            };
 
-            // * try to deserialize JSON that got converted to string
-            // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
+            }));
 
-            // * --- NEWTONSOFT
-            // * basically same thing but with Newtonsoft JSON
-            // * doesn't need options to do what we want
-            IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
+            // // Console.WriteLine(computersJson);
+            // // * --- SYSTEM
+            // // * maps 1 to 1 by default, change naming policy
+            // // * i.e. default: in file property = motherboard, looks to map to model as motherboard (rather than Motherboard)
+            // JsonSerializerOptions options = new JsonSerializerOptions()
+            // {
+            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            // };
 
-            if (computers != null)
-            {
-                foreach (Computer computer in computers)
-                {
-                    // * outputting blanks for Motherboard because deserializer isn't returning Pascal-casing by default, it is trying to map 1 to 1
-                    // Console.WriteLine(computer.Motherboard);
+            // // * try to deserialize JSON that got converted to string
+            // // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
 
-                    // * use loop to run an insert command for each computer
-                    string sql = @"INSERT INTO TutorialAppSchema.Computer (
-                        Motherboard,
-                        HasWifi,
-                        HasLTE,
-                        ReleaseDate,
-                        Price,
-                        VideoCard
-                    ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
-                        + "','" + computer.HasWifi
-                        + "','" + computer.HasLTE
-                        + "','" + computer.ReleaseDate
-                        + "','" + computer.Price
-                        + "','" + EscapeSingleQuote(computer.VideoCard)
-                        + "')";
+            // // * --- NEWTONSOFT
+            // // * basically same thing but with Newtonsoft JSON
+            // // * doesn't need options to do what we want
+            // IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
 
-                    dapper.ExecuteSql(sql);
-                }
-            }
-            // * --- SYSTEM
-            // * create copy and write to file using System
-            // * can reuse options from above to serialize back to camelcase
-            string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computers, options);
-            File.WriteAllText("computersCopySystem.txt", computersCopySystem);
+            // if (computers != null)
+            // {
+            //     foreach (Computer computer in computers)
+            //     {
+            //         // * outputting blanks for Motherboard because deserializer isn't returning Pascal-casing by default, it is trying to map 1 to 1
+            //         // Console.WriteLine(computer.Motherboard);
 
-            // * --- NEWTONSOFT
-            JsonSerializerSettings settings = new JsonSerializerSettings()
-            {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-            };
+            //         // * use loop to run an insert command for each computer
+            //         string sql = @"INSERT INTO TutorialAppSchema.Computer (
+            //             Motherboard,
+            //             HasWifi,
+            //             HasLTE,
+            //             ReleaseDate,
+            //             Price,
+            //             VideoCard
+            //         ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
+            //             + "','" + computer.HasWifi
+            //             + "','" + computer.HasLTE
+            //             + "','" + computer.ReleaseDate
+            //             + "','" + computer.Price
+            //             + "','" + EscapeSingleQuote(computer.VideoCard)
+            //             + "')";
 
-            // * create copy and write to file using Newtonsoft
-            // * without settings, will not be serialized back to camelcase
-            string computersCopy = JsonConvert.SerializeObject(computers, settings);
-            File.WriteAllText("computersCopyNewtonsoft.txt", computersCopy);
+            //         dapper.ExecuteSql(sql);
+            //     }
+            // }
+            // // * --- SYSTEM
+            // // * create copy and write to file using System
+            // // * can reuse options from above to serialize back to camelcase
+            // string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computers, options);
+            // File.WriteAllText("computersCopySystem.txt", computersCopySystem);
+
+            // // * --- NEWTONSOFT
+            // JsonSerializerSettings settings = new JsonSerializerSettings()
+            // {
+            //     ContractResolver = new CamelCasePropertyNamesContractResolver()
+            // };
+
+            // // * create copy and write to file using Newtonsoft
+            // // * without settings, will not be serialized back to camelcase
+            // string computersCopy = JsonConvert.SerializeObject(computers, settings);
+            // File.WriteAllText("computersCopyNewtonsoft.txt", computersCopy);
         }
 
         static string EscapeSingleQuote(string input)
