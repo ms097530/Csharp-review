f3774906c2f87c37ea0489bffde66abb414e0ec3
Michael Schultz <mikewschultz9029@gmail.com>
2023-05-21 16:51:36 -0400

    finished first mapping

--- Intermediate/Program.cs
+++ Intermediate/Program.cs
@@ -81,34 +81,49 @@ namespace Intermediate
                     .ForMember(destination => destination.Motherboard, options => options.MapFrom(source => source.motherboard))
                     .ForMember(destination => destination.VideoCard, options => options.MapFrom(source => source.video_card))
                     .ForMember(destination => destination.ReleaseDate, options => options.MapFrom(source => source.release_date))
+                    // ? could also alter actual mapping if need be... i.e. source => source.price * 0.8m
+                    // ? if destination should be 80% of source price
                     .ForMember(destination => destination.Price, options => options.MapFrom(source => source.price));
             }));
 
-            // // Console.WriteLine(computersJson);
-            // // * --- SYSTEM
-            // // * maps 1 to 1 by default, change naming policy
-            // // * i.e. default: in file property = motherboard, looks to map to model as motherboard (rather than Motherboard)
+            // Console.WriteLine(computersJson);
+            // * --- SYSTEM
+            // * maps 1 to 1 by default, change naming policy
+            // * i.e. default: in file property = motherboard, looks to map to model as motherboard (rather than Motherboard)
             // JsonSerializerOptions options = new JsonSerializerOptions()
             // {
             //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             // };
 
-            // // * try to deserialize JSON that got converted to string
-            // // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
+            // * try to deserialize JSON that got converted to string
+            // IEnumerable<Computer>? computersSystem = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson, options);
+            // * duplicate to work with mapping
+            IEnumerable<ComputerSnake>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computersJson);
 
-            // // * --- NEWTONSOFT
-            // // * basically same thing but with Newtonsoft JSON
-            // // * doesn't need options to do what we want
+            if (computersSystem != null)
+            {
+                IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computersSystem);
+
+                // output Motherboard field from resulting mapped Computers from file with snake-cased JSON
+                foreach (Computer computer in computerResult)
+                {
+                    Console.WriteLine(computer.Motherboard);
+                }
+            }
+
+            // * --- NEWTONSOFT
+            // * basically same thing but with Newtonsoft JSON
+            // * doesn't need options to do what we want
             // IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computersJson);
 
             // if (computers != null)
             // {
             //     foreach (Computer computer in computers)
             //     {
-            //         // * outputting blanks for Motherboard because deserializer isn't returning Pascal-casing by default, it is trying to map 1 to 1
-            //         // Console.WriteLine(computer.Motherboard);
+            // * outputting blanks for Motherboard because deserializer isn't returning Pascal-casing by default, it is trying to map 1 to 1
+            // Console.WriteLine(computer.Motherboard);
 
-            //         // * use loop to run an insert command for each computer
+            // * use loop to run an insert command for each computer
             //         string sql = @"INSERT INTO TutorialAppSchema.Computer (
             //             Motherboard,
             //             HasWifi,
@@ -127,20 +142,20 @@ namespace Intermediate
             //         dapper.ExecuteSql(sql);
             //     }
             // }
-            // // * --- SYSTEM
-            // // * create copy and write to file using System
-            // // * can reuse options from above to serialize back to camelcase
+            // * --- SYSTEM
+            // * create copy and write to file using System
+            // * can reuse options from above to serialize back to camelcase
             // string computersCopySystem = System.Text.Json.JsonSerializer.Serialize(computers, options);
             // File.WriteAllText("computersCopySystem.txt", computersCopySystem);
 
-            // // * --- NEWTONSOFT
+            // * --- NEWTONSOFT
             // JsonSerializerSettings settings = new JsonSerializerSettings()
             // {
             //     ContractResolver = new CamelCasePropertyNamesContractResolver()
             // };
 
-            // // * create copy and write to file using Newtonsoft
-            // // * without settings, will not be serialized back to camelcase
+            // * create copy and write to file using Newtonsoft
+            // * without settings, will not be serialized back to camelcase
             // string computersCopy = JsonConvert.SerializeObject(computers, settings);
             // File.WriteAllText("computersCopyNewtonsoft.txt", computersCopy);
         }
